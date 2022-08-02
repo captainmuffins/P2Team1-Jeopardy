6825c06fd69b1e08abad2209fa1277bd3fcf456b
cruzgsworks <jigs.gisc@gmail.com>
2022-08-01 17:30:14 -0700

    Spring MVC. Created Index Page

---
+++ src/test/java/com/revature/jeopardy/JeopardyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.jeopardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeopardyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
