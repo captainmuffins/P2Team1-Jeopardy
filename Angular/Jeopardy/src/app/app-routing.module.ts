6ddbc2613a3d8cd0b09c1cc612c9cc29aac8c746
Dino Goudas <Constantine930@revature.net>
2022-08-04 13:16:20 -0400

    sign-up page

--- Angular/Jeopardy/src/app/app-routing.module.ts
+++ Angular/Jeopardy/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
   },
+
+  {
+    path: 'SignUp',
+    component:  SignUpComponent
+  }
 ];
 
 @NgModule({
--- Angular/Jeopardy/src/app/app.module.ts
+++ Angular/Jeopardy/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
