50dbdfff51e7971e57126b8311adb90194407f8a
Duncan Stewart <dcs211@scarletmail.rutgers.edu>
2022-08-05 15:16:15 -0400

    delete player by id

--- src/main/java/com/revature/jeopardy/controllers/PlayersController.java
+++ src/main/java/com/revature/jeopardy/controllers/PlayersController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -117,6 +119,13 @@ public class PlayersController {
 		}
 
 		return ResponseEntity.noContent().build();
+		
+	}
+
+	@DeleteMapping(value = "/byId/{id}")
+	public void deletePlayers(@PathVariable int id){
+
+		playersDAO.deleteById(id);
 		
 	}
 
