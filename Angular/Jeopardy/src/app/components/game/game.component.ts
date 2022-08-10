798b57481f0ee909580b2a1603ca422a5d7d0eb8
Dino Goudas <Constantine930@revature.net>
2022-08-10 01:24:51 -0400

    If something is broken... :Sableye

--- Angular/Jeopardy/src/app/components/game/game.component.ts
+++ Angular/Jeopardy/src/app/components/game/game.component.ts
@@ -13,7 +13,7 @@ import { CategorySelectorComponent } from '../category-selector/category-selecto
 })
 export class GameComponent implements OnInit {
   public Bob:Array<number>;
-  
+  public Sab:number=5;
   public a:number = 1;
   public b:number = 2;
   public c:number = 3;
@@ -366,170 +366,216 @@ export class GameComponent implements OnInit {
           },()=>{console.log("it got away")}
           
           );
+          } }
+  /*async Testing(){
+  for(this.i=0;this.i<=4;this.i++){
+   for(this.value;this.value<=5;this.value=this.value+1){
+    this.Js.getQuestionFromApi(this.Bob[this.i], this.value*100).subscribe(
+     (data:any)=>{this.MAN=data.body[0]
+        if(this.C1C1.id==0&&this.i==0){
+          this.C1C1=this.MAN;
+          console.log(this.C1C1)
+        }else if (this.C1C2.id==0&&this.i==0){
+          this.C1C2=this.MAN;
+          console.log(this.C1C2)
+        }else if (this.C1C3.id==0&&this.i==0){
+          this.C1C3=this.MAN;
+          console.log(this.C1C3)
+        }else if (this.C1C4.id==0&&this.i==0){
+          this.C1C4=this.MAN;
+          console.log(this.C1C4)
+        }else if (this.C1C5.id==0&&this.i==0){
+          this.C1C5=this.MAN;
+          console.log(this.C1C5)
           
+        }else if (this.C2C1.id==0&&this.i==1){
+          this.C2C1=this.MAN;
+          console.log(this.C2C1)
+        }else if (this.C2C2.id==0&&this.i==1){
+          this.C2C2=this.MAN;
+          console.log(this.C2C2)
+        }else if (this.C2C3.id==0&&this.i==1){
+          this.C2C3=this.MAN;
+          console.log(this.C2C3)
+        }else if (this.C2C4.id==0&&this.i==1){
+          this.C2C4=this.MAN;
+          console.log(this.C2C4)
+        }else if (this.C2C5.id==0&&this.i==1){
+          this.C2C5=this.MAN;
+        }else if (this.C3C1.id==0&&this.i==2){
+          this.C3C1=this.MAN;
+          console.log(this.C3C1)
+        }else if (this.C3C2.id==0&&this.i==2){
+          this.C3C2=this.MAN;
+          console.log(this.C3C2)
+        }else if (this.C3C3.id==0&&this.i==2){
+          this.C3C3=this.MAN;
+          console.log(this.C3C3)
+        }else if (this.C3C4.id==0&&this.i==2){
+          this.C3C4=this.MAN;
+          console.log(this.C3C4)
+        }else if (this.C3C5.id==0&&this.i==2){
+          this.C3C5=this.MAN;
+          console.log(this.C3C5)
+        }else if (this.C5C1.id==0&&this.i==4){
+          this.C5C1=this.MAN;
+          console.log(this.C5C1)
+        }else if (this.C5C2.id==0&&this.i==4){
+          this.C5C2=this.MAN;
+          console.log(this.C5C2)
+        }else if (this.C5C3.id==0&&this.i==4){
+          this.C5C3=this.MAN;
+          console.log(this.C5C3)
+        }else if (this.C5C4.id==0&&this.i==4){
+          this.C5C4=this.MAN;
+          console.log(this.C5C4)
+        }else if (this.C5C5.id==0&&this.i==4){
+          this.C5C5=this.MAN;
+          console.log(this.C5C5)
+        }else if (this.C4C1.id==0&&this.i==3){
+          this.C4C1=this.MAN;
+          console.log(this.C4C1)
+        }else if (this.C4C2.id==0&&this.i==3){
+          this.C4C2=this.MAN;
+          console.log(this.C4C2)
+        }else if (this.C4C3.id==0&&this.i==3){
+          this.C4C3=this.MAN;
+          console.log(this.C4C3)
+        }else if (this.C4C4.id==0&&this.i==3){
+          this.C4C4=this.MAN;
+          console.log(this.C4C4)
+        }else if (this.C4C5.id==0&&this.i==3){
+          this.C4C5=this.MAN;
+          console.log(this.C4C5)
+        }else{this.value=1;}
+      },()=>{console.log("it got away")});
+    } this.value=1;
+    console.log(this.i)
+  } 
+    }*/
+ async Test2(){
+  if (this.Sab>=1){
+for(this.value;this.value<=5;this.value=this.value+1){
+    this.Js.getQuestionFromApi(this.Bob[0], this.value*100).subscribe(
+      (data:any)=>{this.MAN=data.body[0]
+        if(this.C1C1.id==0){
+          this.C1C1=this.MAN;
+          console.log(this.C1C1)
+        }else if (this.C1C2.id==0){
+          this.C1C2=this.MAN;
+          console.log(this.C1C2)
+        }else if (this.C1C3.id==0){
+          this.C1C3=this.MAN;
+          console.log(this.C1C3)
+        }else if (this.C1C4.id==0){
+          this.C1C4=this.MAN;
+          console.log(this.C1C4)
+        }else if (this.C1C5.id==0){
+          this.C1C5=this.MAN;
+          console.log(this.C1C5)
+          this.value=1;
         }
-    this.D1=this.C1C1;
-    this.D2=this.C1C2;
-    this.D3=this.C1C3;
-    this.D4=this.C1C4;
-    this.D5=this.C1C5;
-    for(this.j=0;this.j<10;this.j++){
-      if(this.D1.value<this.D2.value)
-      {}else if(this.D1.value>this.D2.value){
-        this.D6=this.D1
-        this.D1=this.D2
-        this.D2=this.D6
-      }if(this.D2.value<this.D3.value)
-      {}else if(this.D2.value>this.D3.value){
-        this.D6=this.D2
-        this.D2=this.D3
-        this.D3=this.D6
-      }if(this.D3.value<this.D4.value)
-      {}else if(this.D3.value>this.D4.value){
-        this.D6=this.D3
-        this.D3=this.D4
-        this.D4=this.D6
-      }if(this.D4.value<this.D5.value)
-      {}else if(this.D4.value>this.D5.value){
-        this.D6=this.D4
-        this.D4=this.D5
-        this.D5=this.D6
+      },()=>{console.log("it got away")});
+    }this.value=1;}
+  if(this.Sab>=2){
+  for(this.value;this.value<=5;this.value=this.value+1){
+  this.Js.getQuestionFromApi(this.Bob[1], this.value*100).subscribe(
+    (data:any)=>{this.MAN=data.body[0]
+      if(this.C2C1.id==0){
+        this.C2C1=this.MAN;
+        console.log(this.C2C1)
+      } else if (this.C2C2.id==0){
+        this.C2C2=this.MAN;
+        console.log(this.C2C2)
+      }else if (this.C2C3.id==0){
+        this.C2C3=this.MAN;
+        console.log(this.C2C3)
+      }else if (this.C2C4.id==0){
+        this.C2C4=this.MAN;
+        console.log(this.C2C4)
+      }else if (this.C2C5.id==0){
+        this.C2C5=this.MAN;
+        console.log(this.C2C5)
+      }
+    },()=>{console.log("it got away")}
+    
+    );
+    
+  } this.value=1;}
+  if(this.Sab>=3){
+for(this.value;this.value<=5;this.value=this.value+1){
+    this.Js.getQuestionFromApi(this.Bob[2], this.value*100).subscribe(
+      (data:any)=>{this.MAN=data.body[0]
+        if(this.C3C1.id==0){
+          this.C3C1=this.MAN;
+          console.log(this.C3C1)
+        } else if (this.C3C2.id==0){
+          this.C3C2=this.MAN;
+          console.log(this.C3C2)
+        }
+        else if (this.C3C3.id==0){
+          this.C3C3=this.MAN;
+          console.log(this.C3C3)
+        }else if (this.C3C4.id==0){
+          this.C3C4=this.MAN;
+          console.log(this.C3C4)
+        }else if (this.C3C5.id==0){
+          this.C3C5=this.MAN;
+          console.log(this.C3C5)
+        }
+      },()=>{console.log("it got away")}
+      
+      );
+    }this.value=1;}
+  if(this.Sab>=5){
+    for(this.value;this.value<=5;this.value=this.value+1){
+  this.Js.getQuestionFromApi(this.Bob[3], this.value*100).subscribe(
+    (data:any)=>{this.MAN=data.body[0]
+      if(this.C5C1.id==0){
+        this.C5C1=this.MAN;
+        console.log(this.C5C1)
+      } else if (this.C5C2.id==0){
+        this.C5C2=this.MAN;
+        console.log(this.C5C2)
+      }else if (this.C5C3.id==0){
+        this.C5C3=this.MAN;
+        console.log(this.C5C3)
+      }else if (this.C5C4.id==0){
+        this.C5C4=this.MAN;
+        console.log(this.C5C4)
+      }else if (this.C5C5.id==0){
+        this.C5C5=this.MAN;
+        console.log(this.C5C5)
+      }
+    },()=>{console.log("it got away")}
+    
+    );  
+  }this.value=1;}
+  if(this.Sab>=4){
+  for(this.value;this.value<=5;this.value=this.value+1){
+    this.Js.getQuestionFromApi(this.Bob[4], this.value*100).subscribe(
+      (data:any)=>{this.MAN=data.body[0]
+        if(this.C4C1.id==0){
+          this.C4C1=this.MAN;
+          console.log(this.C4C1)
+        } else if (this.C4C2.id==0){
+          this.C4C2=this.MAN;
+          console.log(this.C4C2)
+        }else if (this.C4C3.id==0){
+          this.C4C3=this.MAN;
+          console.log(this.C4C3)
+        }else if (this.C4C4.id==0){
+          this.C4C4=this.MAN;
+          console.log(this.C4C4)
+        }else if (this.C4C5.id==0){
+          this.C4C5=this.MAN;
+          console.log(this.C4C5)
+        }
+      },()=>{console.log("it got away")}
+      
+      );
       }}
-    this.C1C1=this.D1;
-    this.C1C2=this.D2;
-    this.C1C3=this.D3;
-    this.C1C4=this.D4;
-    this.C1C5=this.D5;
-    this.D1=this.C2C1;
-    this.D2=this.C2C2;
-    this.D3=this.C2C3;
-    this.D4=this.C2C4;
-    this.D5=this.C2C5;
-    for(this.j=0;this.j<10;this.j++){
-      if(this.D1.value<this.D2.value)
-      {}else if(this.D1.value>this.D2.value){
-        this.D6=this.D1
-        this.D1=this.D2
-        this.D2=this.D6
-      }if(this.D2.value<this.D3.value)
-      {}else if(this.D2.value>this.D3.value){
-        this.D6=this.D2
-        this.D2=this.D3
-        this.D3=this.D6
-      }if(this.D3.value<this.D4.value)
-      {}else if(this.D3.value>this.D4.value){
-        this.D6=this.D3
-        this.D3=this.D4
-        this.D4=this.D6
-      }if(this.D4.value<this.D5.value)
-      {}else if(this.D4.value>this.D5.value){
-        this.D6=this.D4
-        this.D4=this.D5
-        this.D5=this.D6
-      }}
-    this.C2C1=this.D1;
-    this.C2C2=this.D2;
-    this.C2C3=this.D3;
-    this.C2C4=this.D4;
-    this.C2C5=this.D5;
-    this.D1=this.C3C1;
-    this.D2=this.C3C2;
-    this.D3=this.C3C3;
-    this.D4=this.C3C4;
-    this.D5=this.C3C5;
-    for(this.j=0;this.j<10;this.j++){
-      if(this.D1.value<this.D2.value)
-      {}else if(this.D1.value>this.D2.value){
-        this.D6=this.D1
-        this.D1=this.D2
-        this.D2=this.D6
-      }if(this.D2.value<this.D3.value)
-      {}else if(this.D2.value>this.D3.value){
-        this.D6=this.D2
-        this.D2=this.D3
-        this.D3=this.D6
-      }if(this.D3.value<this.D4.value)
-      {}else if(this.D3.value>this.D4.value){
-        this.D6=this.D3
-        this.D3=this.D4
-        this.D4=this.D6
-      }if(this.D4.value<this.D5.value)
-      {}else if(this.D4.value>this.D5.value){
-        this.D6=this.D4
-        this.D4=this.D5
-        this.D5=this.D6
-      }}
-    this.C3C1=this.D1;
-    this.C3C2=this.D2;
-    this.C3C3=this.D3;
-    this.C3C4=this.D4;
-    this.C3C5=this.D5;
-    this.D1=this.C4C1;
-    this.D2=this.C4C2;
-    this.D3=this.C4C3;
-    this.D4=this.C4C4;
-    this.D5=this.C4C5;
-    for(this.j=0;this.j<10;this.j++){
-      if(this.D1.value<this.D2.value)
-      {}else if(this.D1.value>this.D2.value){
-        this.D6=this.D1
-        this.D1=this.D2
-        this.D2=this.D6
-      }if(this.D2.value<this.D3.value)
-      {}else if(this.D2.value>this.D3.value){
-        this.D6=this.D2
-        this.D2=this.D3
-        this.D3=this.D6
-      }if(this.D3.value<this.D4.value)
-      {}else if(this.D3.value>this.D4.value){
-        this.D6=this.D3
-        this.D3=this.D4
-        this.D4=this.D6
-      }if(this.D4.value<this.D5.value)
-      {}else if(this.D4.value>this.D5.value){
-        this.D6=this.D4
-        this.D4=this.D5
-        this.D5=this.D6
-      }}
-    this.C4C1=this.D1;
-    this.C4C2=this.D2;
-    this.C4C3=this.D3;
-    this.C4C4=this.D4;
-    this.C4C5=this.D5;
-    this.D1=this.C5C1;
-    this.D2=this.C5C2;
-    this.D3=this.C5C3;
-    this.D4=this.C5C4;
-    this.D5=this.C5C5;
-    for(this.j=0;this.j<10;this.j++){
-      if(this.D1.value<this.D2.value)
-      {}else if(this.D1.value>this.D2.value){
-        this.D6=this.D1
-        this.D1=this.D2
-        this.D2=this.D6
-      }if(this.D2.value<this.D3.value)
-      {}else if(this.D2.value>this.D3.value){
-        this.D6=this.D2
-        this.D2=this.D3
-        this.D3=this.D6
-      }if(this.D3.value<this.D4.value)
-      {}else if(this.D3.value>this.D4.value){
-        this.D6=this.D3
-        this.D3=this.D4
-        this.D4=this.D6
-      }if(this.D4.value<this.D5.value)
-      {}else if(this.D4.value>this.D5.value){
-        this.D6=this.D4
-        this.D4=this.D5
-        this.D5=this.D6
-      }}
-    this.C5C1=this.D1;
-    this.C5C2=this.D2;
-    this.C5C3=this.D3;
-    this.C5C4=this.D4;
-    this.C5C5=this.D5;
-  }
-
+ }
   public D1:Clue={
     id:0,
     answer:"",
@@ -777,7 +823,8 @@ export class GameComponent implements OnInit {
 
   
   ngOnInit(): void {
-    this.StartGame();
+    //this.StartGame();
+    this.Test2();
   }
 
 }
