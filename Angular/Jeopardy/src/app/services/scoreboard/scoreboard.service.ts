05862a1acefc146fefa15a566544712aed5b92c5
Kevin Trinh <kevintrinh388@gmail.com>
2022-08-10 21:14:43 -0400

    updated scoreboard service

--- Angular/Jeopardy/src/app/services/scoreboard/scoreboard.service.ts
+++ Angular/Jeopardy/src/app/services/scoreboard/scoreboard.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScoreboardService {
+  private _sessions = 'http://localhost:8080/api/session/';
+  private _playerSessions = 'http://localhost:8080/api/session/byPlayer/';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllSessions() {
+    return this.http.get<any>(this._sessions, {
+      withCredentials: true,
+    });
+  }
+  getPlayerSessions(playerId: any) {
+    return this.http.get<any>(this._playerSessions + playerId, {
+      withCredentials: true,
+    });
+  }
 }
