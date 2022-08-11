7aee339513ad5c3959ae56a0b5b4760075553d73
Kevin Trinh <kevintrinh388@gmail.com>
2022-08-10 20:43:51 -0400

    created SessionDTO, updated PostMapping for SessionController

--- src/main/java/com/revature/jeopardy/controllers/SessionController.java
+++ src/main/java/com/revature/jeopardy/controllers/SessionController.java
@@ -1,6 +1,7 @@
 package com.revature.jeopardy.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -13,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.jeopardy.daos.GamesDAO;
+import com.revature.jeopardy.daos.PlayersDAO;
 import com.revature.jeopardy.daos.SessionDAO;
 import com.revature.jeopardy.dtos.Response;
+import com.revature.jeopardy.dtos.SessionDTO;
+import com.revature.jeopardy.models.Games;
+import com.revature.jeopardy.models.Players;
 import com.revature.jeopardy.models.Session;
 
 
@@ -25,13 +31,28 @@ public class SessionController {
     
     @Autowired
     private SessionDAO sessionDAO;
-
-
+    @Autowired
+    private PlayersDAO playerDAO;
+    @Autowired
+    private GamesDAO gameDAO;
 
     @PostMapping
-    public ResponseEntity<Session> addSession(@RequestBody Session session){
+    public ResponseEntity<Session> addSession(@RequestBody SessionDTO sessionDTO){
 
         try {
+            // Use DTO, find player, game by Id, set game and player
+            // save session
+            Session session = new Session();
+            session.setSessionWinnings(sessionDTO.getSessionWinnings());
+            session.setSessionWinner(sessionDTO.isSessionWinner());
+
+            Optional<Players> playerOptional = playerDAO.findById(sessionDTO.getSessionPlayerfk());
+            Players player = playerOptional.get();
+            session.setSessionPlayerfk(player);
+
+            Optional<Games> gameOptional = gameDAO.findById(sessionDTO.getSessionGamefk());
+            Games game = gameOptional.get();
+            session.setSessionGamefk(game);
 
             Session newSession = sessionDAO.save(session);
 
