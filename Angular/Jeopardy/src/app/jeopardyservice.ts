5ba03b906fbaf7ed898ca64bb74da2f8c4483fdb
cruzgsworks <jigs.gisc@gmail.com>
2022-08-15 08:31:08 -0700

    fix fixed mode http -> https

--- Angular/Jeopardy/src/app/jeopardyservice.ts
+++ Angular/Jeopardy/src/app/jeopardyservice.ts
@@ -6,30 +6,34 @@ import { Title } from '@angular/platform-browser';
 import { Clue } from './models/clue/clue';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class jeopardyservice {
-
-  constructor(private http:HttpClient) { }
-  Category:Category={
-     id:0,
-     title:"",
-     clues_count:0
+  constructor(private http: HttpClient) {}
+  Category: Category = {
+    id: 0,
+    title: '',
+    clues_count: 0,
   };
 
-  Clue:Clue={
-    id:0,
-    answer:"",
-    question:"",
-    value:0,
-    category_id:0
+  Clue: Clue = {
+    id: 0,
+    answer: '',
+    question: '',
+    value: 0,
+    category_id: 0,
+  };
+  //HOPEFULLY this gets a proper category as a response.
+  //it says I need an Id to get a category not sure if it needs it like this or if the int raw can do.
+  getCategoryFromApi(id: number): Observable<HttpResponse<Category>> {
+    return this.http.get('https://jservice.io/api/category?id=' + id, {
+      observe: 'response',
+    }) as Observable<HttpResponse<Category>>;
   }
-//HOPEFULLY this gets a proper category as a response.
-//it says I need an Id to get a category not sure if it needs it like this or if the int raw can do.
-  getCategoryFromApi(id:number):Observable<HttpResponse<Category>>{
-    return this.http.get("http://jservice.io/api/category?id="+id,{observe:"response"}) as Observable<HttpResponse<Category>>
-  }
-  getQuestionFromApi(Gem:number,i:number):Observable<HttpResponse<Clue>>{
-    return this.http.get("http://jservice.io/api/clues?value="+i+"&category="+Gem,{observe:"response"}) as Observable<HttpResponse<Clue>>
+  getQuestionFromApi(Gem: number, i: number): Observable<HttpResponse<Clue>> {
+    return this.http.get(
+      'https://jservice.io/api/clues?value=' + i + '&category=' + Gem,
+      { observe: 'response' }
+    ) as Observable<HttpResponse<Clue>>;
   }
 }
